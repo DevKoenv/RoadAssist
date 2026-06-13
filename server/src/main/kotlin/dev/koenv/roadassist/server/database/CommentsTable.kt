785a7c76775ff49c357629b97950b3d1820a0dd1
DevKoenv <koenvanvlimmeren@gmail.com>
2026-06-13 22:03:31 +0200

    fix(db): give CommentsTable.type a DB default so ALTER TABLE succeeds on existing SQLite databases

--- server/src/main/kotlin/dev/koenv/roadassist/server/database/CommentsTable.kt
+++ server/src/main/kotlin/dev/koenv/roadassist/server/database/CommentsTable.kt
@@ -7,7 +7,7 @@ import org.jetbrains.exposed.v1.core.dao.id.IntIdTable
 object CommentsTable : IntIdTable("comments") {
     val incidentId = reference("incident_id", IncidentsTable)
     val authorRole = enumerationByName<AuthorRole>("author_role", 20)
-    val type = enumerationByName<CommentType>("type", 20)
+    val type = enumerationByName<CommentType>("type", 20).default(CommentType.MESSAGE)
     val content = text("content")
     val createdAt = varchar("created_at", 50)
 }
